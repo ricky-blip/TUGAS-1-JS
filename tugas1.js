b9851efe49972f2e14e50ecfa93775d6ab0849e3
ricky-blip <ricky.rinaldy77@gmail.com>
2022-04-25 19:18:12 +0700

    tugas 1 Javascript

---
+++ tugas1.js
@@ -0,0 +1,9 @@
+var namaDepan = "Ricky"
+let namaAkhir = "Rinaldy"
+const umur = 25
+
+console.log("Nama Depan = ", namaDepan)
+console.log("Nama Akhir = ", namaAkhir)
+console.log("Umur       = ", umur , "\n")
+
+console.log(namaDepan , namaAkhir , umur , " Tahun")
